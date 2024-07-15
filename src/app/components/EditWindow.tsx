5107400f90f25ae4d89c57dc3ea502f77bd0d85c
Tomáš Krže <106106200+TomasKsk@users.noreply.github.com>
2024-07-15 21:30:29 +0200

    Finished Edit window functions
    Functions added:
    
    Handle Tags - to autocomplete tag user input, replace white space with comma
    
    Handle Tag splitting - remove empty array items
    
    Changed checkbox items to buttons for better appeareance

--- src/app/components/EditWindow.tsx
+++ src/app/components/EditWindow.tsx
@@ -74,6 +74,25 @@ const EditWindow: React.FC<Props> = ({ todo, setTodoList, editingTodo, setEditin
         }
     };
 
+    const handleTags = (e: string) => {
+        // auto replace white space and # character with ,
+        const tags = e.replace(/\s|#/g, ',').split(',').map(a => a.trim())
+        console.log(tags)
+        setForm(prev => ({
+            ...prev,
+            tags: tags
+        }));
+    }
+
+    // on blur filter out the empty array items
+    const handleTagsSplitting = () => {
+        const tags = form.tags.filter(a => a.length > 0)
+        setForm(prev => ({
+            ...prev,
+            tags: tags
+        }));
+    }
+ 
     const handleUpdate = async(id: string, fields: Partial<Todo>) => {
         try {
             const updatedTodo = await updateTodo(id, fields);
@@ -89,70 +108,81 @@ const EditWindow: React.FC<Props> = ({ todo, setTodoList, editingTodo, setEditin
     }
 
     return (
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[95%] h-[70%] bg-white rounded-xl p-4 shadow-lg">
-            <div className="flex flex-row w-full items-center justify-between mb-4">
-                <h2 className="flex flex-col text-lg font-semibold items-center flex-grow">
-                    Edit your todo no: {editingTodo.id}
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[95%] bg-white rounded-xl shadow-xl border-4 border-gray-200 drop-shadow-[0_1.5px_1.5px_rgba(0,0,0,0.8)]">
+            <div className="flex flex-row w-full items-center justify-between bg-gray-400 rounded-t-md">
+                <h2 className="flex flex-col text-lg font-semibold items-center flex-grow py-2 px-4">
+                    Edit your todo
                 </h2>
-                <button className="font-black text-xl" onClick={handleClose}>
+                <button className="font-black text-xl p-2" onClick={handleClose}>
                     X
                 </button>
             </div>
 
-            <div className="flex flex-col">
+            <div className="flex flex-col p-4">
                 {/* {todo ? todo.id : 'Loading...'} */}
                 {
                     form ?
-                        <div className="flex flex-col gap-4 items-center ">
+                        <div className="flex flex-col gap-8 items-center">
                             <input 
                                 type="text"
                                 value={form.text}
                                 onChange={(e) => handleChange('text', e)}
                                 onKeyDown={handleKeyDown}
-                                className="w-full flex px-3 rounded-md py-2 bg-gray-200"
+                                className="w-full flex px-4 py-3 rounded-md bg-gray-200 text-xl"
                             />
 
-                            {/* priority checkbox */}
                             <div className="flex flex-row w-full items-center justify-around text-xl">
-                                <label htmlFor="priorityCheckbox">
+                                {/* priority checkbox */}
+                                <button 
+                                    style={{backgroundColor: form.priority ? 'rgb(250 204 21)' : 'rgb(243 244 246)'}} 
+                                    className="px-6 py-3 bg-gray-100 rounded-md drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
+                                    onClick={() => handleCheckBox('priority')}
+                                >
                                     Priority
-                                </label>
-                                <input className="size-[20px]" 
-                                    id="priorityCheckbox"
-                                    type="checkbox"
-                                    checked={form.priority}
-                                    onChange={() => handleCheckBox('priority')}
-                                />
-                            </div>
+                                </button>
 
-                            {/* complete checkbox */}
-                            <div className="flex flex-row w-full items-center justify-around text-xl">
-                                <label htmlFor="completeCheckbox">
+                                {/* complete checkbox */}
+                                <button 
+                                    style={{backgroundColor: form.complete ? 'rgb(250 204 21)' : 'rgb(243 244 246)'}} 
+                                    className="px-6 py-3 bg-gray-100 rounded-md drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
+                                    onClick={() => handleCheckBox('complete')}
+                                >
                                     Complete
-                                </label>
-                                <input className="size-[20px]" 
-                                    id="completeCheckbox"
-                                    type="checkbox"
-                                    checked={form.complete}
-                                    onChange={() => handleCheckBox('complete')}
-                                />
+                                </button>
                             </div>
 
                             {/* Due Date input */}
                             <div className="flex flex-row w-full items-center justify-around text-xl">
-                                <label htmlFor="completeCheckbox">
+                                <label htmlFor="dueDate">
                                     Due Date
                                 </label>
                                 <input
-                                    id=""
+                                    id="dueDate"
                                     type="date"
                                     value={form.duedate}
                                     onChange={(e) => handleChange('duedate',e)}
                                 />
                             </div>
                             
+                            <div className="flex flex-row w-full items-center justify-around text-md">
+                                <label htmlFor="tagsArray">
+                                    #Tags
+                                </label>
+                                <input
+                                    id="tagsArray"
+                                    type="text"
+                                    value={form.tags}
+                                    onChange={(e) => handleTags(e.target.value)}
+                                    onBlur={handleTagsSplitting}
+                                    onKeyDown={handleKeyDown}
+                                    className="w-full flex px-3 py-2 rounded-md bg-gray-200"
+                                />
+                            </div>
                             
-                            
+                            <div className="invisible px-6 py-2 rounded-md bottom-4 bg-gray-300">
+                                Save Changes
+                            </div>
+
                             <button onClick={() => handleSave(form.id)} className="absolute px-6 py-2 rounded-md bottom-4 bg-gray-300">
                                 Save Changes
                             </button>
